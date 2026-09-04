aeee63fe8f7a46c1deee9202886456ffb8da7b72
carlosmedinamateo <carlosmedinamateo@gmail.com>
2026-09-05 00:02:49 +0100

    feat: Configurar site para rutas de assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sunneacademy.es',
   vite: {
     plugins: [tailwindcss()],
   },
